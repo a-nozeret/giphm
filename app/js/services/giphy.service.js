21dda54661818ec3ab8f8a9cacb3fe3e134ed9ab
Antoine Nozeret <antoine.nozeret@gmail.com>
2017-02-03 18:14:02 +0100

    Use https for giphy api url

--- app/js/services/giphy.service.js
+++ app/js/services/giphy.service.js
@@ -4,7 +4,7 @@ GiphyService.$inject = ['$http'];
 function GiphyService($http) {
     var _this = this;
     _this.getRandom = function getRandom(query) {
-        const url = 'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=' + query;
+        const url = 'https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=' + query;
         return $http.get(url);
     }
 }
